aa3c6294c6fa07528c27fcf078ff8d9a52eff10d
Nitin Tank <nitintank3@gmail.com>
2023-09-06 16:33:19 +0530

    Changes

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ const submenuItems = document.querySelectorAll(".submenu_item");
 const sidebarOpen = document.querySelector("#sidebarOpen");
 const sidebarClose = document.querySelector(".collapse_sidebar");
 const sidebarExpand = document.querySelector(".expand_sidebar");
-sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+// sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+let arrow = document.querySelectorAll(".arrow");
+// for (var i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", (e)=>{
+//  let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+//  arrowParent.classList.toggle("showMenu");
+//   });
+// }
+let sidebarBtn = document.querySelector(".bx-menu");
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("showMenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("showMenu");
+      }
+    });
+  });
+});
 
 // sidebarClose.addEventListener("click", () => {
 //   sidebar.classList.add("close", "hoverable");
@@ -14,16 +37,16 @@ sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
 //   sidebar.classList.remove("close", "hoverable");
 // });
 
-sidebar.addEventListener("mouseenter", () => {
-  if (sidebar.classList.contains("hoverable")) {
-    sidebar.classList.remove("close");
-  }
-});
-sidebar.addEventListener("mouseleave", () => {
-  if (sidebar.classList.contains("hoverable")) {
-    sidebar.classList.add("close");
-  }
-});
+// sidebar.addEventListener("mouseenter", () => {
+//   if (sidebar.classList.contains("hoverable")) {
+//     sidebar.classList.remove("close");
+//   }
+// });
+// sidebar.addEventListener("mouseleave", () => {
+//   if (sidebar.classList.contains("hoverable")) {
+//     sidebar.classList.add("close");
+//   }
+// });
 
 // darkLight.addEventListener("click", () => {
 //   body.classList.toggle("dark");
@@ -35,22 +58,22 @@ sidebar.addEventListener("mouseleave", () => {
 //   }
 // });
 
-submenuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("show_submenu");
-    submenuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show_submenu");
-      }
-    });
-  });
-});
+// submenuItems.forEach((item, index) => {
+//   item.addEventListener("click", () => {
+//     item.classList.toggle("show_submenu");
+//     submenuItems.forEach((item2, index2) => {
+//       if (index !== index2) {
+//         item2.classList.remove("show_submenu");
+//       }
+//     });
+//   });
+// });
 
-if (window.innerWidth < 790) {
-  sidebar.classList.add("close");
-} else {
-  sidebar.classList.remove("close");
-}
+// if (window.innerWidth < 790) {
+//   sidebar.classList.add("close");
+// } else {
+//   sidebar.classList.remove("close");
+// }
 
 // Popup Javascript
 
